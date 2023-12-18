4600f587f30354cec34f2ac82094e3e0ee420b47
Petra Ingemarsson <petra.ingemarsson@gmail.com>
2023-12-18 10:27:49 +0100

    edit and delete exercises

--- src/components/admin.component.js
+++ src/components/admin.component.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Logout from "./logout.component";
 import axiosService from "../services/axios.service";
 
@@ -6,13 +6,24 @@ export default function Admin() {
     const [exercisename, setExercisename] = useState("");
     const [description, setDescription] = useState("");
     const [file, setFile] = useState(null);
-    const [error, setError] = useState("");
+    const [addError, setAddError] = useState("");
+    const [successMessage, setSuccessMessage] = useState("");
+    const [exercises, setExercises] = useState([]);
+    const [selectedExercise, setSelectedExercise] = useState(null);
+    const [editingExercisename, setEditingExercisename] = useState("");
+    const [editingDescription, setEditingDescription] = useState("");
+    const [updateError, setUpdateError] = useState("");
+    const [deleteError, setDeleteError] = useState("");
+
+    useEffect(() => {
+        getExercises();
+    }, []);
 
     const handleAddExercise = async (e) => {
         e.preventDefault();
 
         try {
-            setError("");
+            setAddError("");
 
             const formData = new FormData();
             formData.append("exercisename", exercisename);
@@ -35,14 +46,104 @@ export default function Admin() {
             // Nollställer formuläret
             e.target.reset();
 
+            // Hämtar uppdaterade övningar
+            getExercises();
+
+            // Uppdaterar meddelandet
+            setSuccessMessage("Övningen har lagts till!");
+
+            // Tar bort meddelandet efter 5 sekunder
+            setTimeout(() => {
+                setSuccessMessage("");
+            }, 5000);
+
         } catch (error) {
             console.error("Felmeddelande:", error);
-            setError(error.response?.data.message || "Ett fel uppstod vid tillägg av övning!");
+            setAddError(error.response?.data.message || "Ett fel uppstod vid tillägg av övning!");
+
+            // Tar bort meddelandet efter 5 sekunder
+            setTimeout(() => {
+                setAddError("");
+            }, 5000);
         }
     }
 
     const handleFileChange = (e) => {
         setFile(e.target.files[0]);
+    }
+
+    const getExercises = async () => {
+        try {
+            const response = await axiosService.get("/exercises");
+            setExercises(response.data);
+        } catch (error) {
+            console.error("Felmeddelande:", error);
+        }
+    }
+
+    const handleUpdateExercise = async () => {
+        try {
+            setUpdateError("");
+
+            const updateData = {
+                exercisename: editingExercisename,
+                description: editingDescription,
+            }
+
+            const response = await axiosService.put("/exercises/" + selectedExercise._id, updateData);
+
+            console.log("Övning uppdaterad:", response.data);
+
+            // Återställer tillståndsvariabler efter framgångsrikt put-anrop
+            setEditingExercisename("");
+            setEditingDescription("");
+            setSelectedExercise(null);
+
+            // Hämtar uppdaterade övningar
+            getExercises();
+
+        } catch (error) {
+            console.error("Felmeddelande:", error);
+            setUpdateError(error.response?.data.message || "Ett fel uppstod vid uppdatering av övning!");
+
+            // Tar bort meddelandet efter 5 sekunder
+            setTimeout(() => {
+                setUpdateError("");
+            }, 5000);
+        }
+    }
+
+    const handleSelectExercise = (exercise) => {
+        setEditingExercisename(exercise.exercisename);
+        setEditingDescription(exercise.description);
+        setSelectedExercise(exercise);
+    }
+
+    // Avbryter redigeringsläge
+    const handleCancelEdit = () => {
+        setEditingExercisename("");
+        setEditingDescription("");
+        setSelectedExercise(null);
+    }
+
+    const handleDeleteExercise = async (exerciseId) => {
+        try {
+            setDeleteError("");
+
+            const userConfirmed = window.confirm("Är du säker på att du vill radera övningen?");
+
+            if (userConfirmed) {
+                const response = await axiosService.delete("/exercises/" + exerciseId);
+
+                console.log("Övning raderad:", response.data);
+
+                // Hämtar uppdaterade övningar
+                getExercises();
+            }
+        } catch (error) {
+            console.error("Felmeddelande:", error);
+            setDeleteError(error.response?.data.message || "Ett fel uppstod vid radering av övning!");
+        }
     }
 
     return (
@@ -79,7 +180,54 @@ export default function Admin() {
                 <br />
                 <button type="submit">Spara</button>
             </form>
-            {error && <p style={{ color: "red" }}>{error}</p>}
+            {addError && <p style={{ color: "red" }}>{addError}</p>}
+            {successMessage && <p style={{ color: "green" }}>{successMessage}</p>}
+            <h2>Befintliga övningar</h2>
+
+            {exercises.map((exercise) => (
+                <div key={exercise._id}>
+                    {selectedExercise === exercise ? (
+                        <>
+                            <h3>Redigera övning</h3>
+                            <label>
+                                Namn:
+                                <br />
+                                <input
+                                    type="text"
+                                    value={editingExercisename}
+                                    onChange={(e) => setEditingExercisename(e.target.value)}
+                                />
+                            </label>
+                            <br />
+                            <label>
+                                Beskrivning:
+                                <br />
+                                <textarea
+                                    value={editingDescription}
+                                    onChange={(e) => setEditingDescription(e.target.value)}
+                                />
+                            </label>
+                            <br />
+                            {updateError && <p style={{ color: "red" }}>{updateError}</p>}
+                            <button onClick={handleUpdateExercise}>Spara</button>
+                            <button onClick={handleCancelEdit}>Avbryt</button>
+                        </>
+                    ) : (
+                        <>
+                            <h3>{exercise.exercisename}</h3>
+                            <p>{exercise.description}</p>
+                            <video src={`http://localhost:3050/uploads/${exercise.filename}`} width={100} controls>
+                                Din webbläsare kan inte visa videon...
+                            </video>
+                            <br />
+                            <button onClick={() => handleSelectExercise(exercise)}>Redigera</button>
+                            <button onClick={() => handleDeleteExercise(exercise._id)}>Radera</button>
+                            {deleteError && <p style={{ color: "red" }}>{deleteError}</p>}
+                        </>
+                    )}
+                </div>
+            ))}
+
         </div>
     );
 }
